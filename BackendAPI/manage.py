144c85bc31b9126a3fb98ba41a587f658835a7a4
ShiinaOrez <shiina_orez@qq.com>
2019-02-02 23:19:43 +0800

    [fixed] add function info

--- BackendAPI/manage.py
+++ BackendAPI/manage.py
@@ -2,9 +2,10 @@ import sys
 import importlib
 import os
 import time
+import data_an
 from spy_eye_app import create_app, db
 from spy_eye_app.models import User, App, User2App, Oper, Coor
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Command
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -21,6 +22,13 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 @manager.command
 def createdb():
     db.create_all()
+
+class info(Command):
+    def run(self):
+        data_an.run()
+        print("<!--Over-->")
+
+manager.add_command('info', info())
 
 if __name__ == "__main__":
     manager.run()
